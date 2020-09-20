d7fe1133d380b60f65c8d2efe9862fbd66eb06b0
Leomf123 <64655819+Leomf123@users.noreply.github.com>
2020-09-20 17:42:40 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "Populacao.h"
+#include "OP.h"
+#include "Popul_EE.h"
+#include "EEv.h"
+#include "SA.h"
+
+
+
+
+using namespace std;
+
+
+
+int main()
+{
+
+
+
+
+     // GA();
+       //EE();
+       RS();
+
+
+
+   //algoritmo_genetico();
+   //est_evol();
+ //SA(1e5,1e-10,0.995);
+
+
+
+
+    return 0;
+}
